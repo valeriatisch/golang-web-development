4c93e2b6374b893bdf1659ea40c28daa24bb0588
Valeria Tisch <valeria.tisch97@gmail.com>
2023-09-09 11:21:59 +0200

    added todo for init module first

--- exercises/02_fetch_data/main.go
+++ exercises/02_fetch_data/main.go
@@ -4,9 +4,14 @@ import (
 	// "github.com/joho/godotenv"
 )
 
-// 1) TODO: Create a constant for the number of activities to fetch
+// 1) TODO: Initialize a module for this exercise first by using
+// 		go mod init modulename
+// 		go mod tidy
+// Also add it to your go.work file!
 
-// 2) TODO:
+// 2) TODO: Create a constant for the number of activities to fetch
+
+// 3) TODO:
 // 		a) You need to load the env file
 // 		b) Connect to the database
 // 		c) Make sure the table you want to put your data in exists
